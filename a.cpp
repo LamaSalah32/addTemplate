bfb9be1191f11d010aaa4a77fa15a12a635e243b
lamasalah32 <lamasalah32@gmail.com>
2023-03-12 02:48:21 +0200

    update

--- a.cpp
+++ a.cpp
@@ -1,35 +1,24 @@
 #include <bits/stdc++.h>
-#include "addTemplate.cpp"
-#define ll long long
-#define ull unsigned long long
-#define all(vec) vec.begin(), vec.end()
-#define cin(v) for (auto& i : v) cin >> i
-#define cout(v) for (auto& i : v) cout << i << " "; cout << "\n"
-#define Ceil(a, b) ((a / b) + (a % b ? 1 : 0))
-#define mod 998244353
-#define fr first
-#define sc second
+
+#ifndef ONLINE_JUDGE
+#include "addTemplate.h"
+#endif
 
 using namespace std ;
 
-void code (){
-    ios_base :: sync_with_stdio (false), cin.tie(0), cout.tie(0);
-  //  freopen("input.txt", "r", stdin);
-  //  freopen("output.txt", "w", stdout);
+
+void addTemp (string s, string t){
+#ifndef ONLINE_JUDGE
+    addTemplate(s, t);
+#endif
 }
 
 void solve(){
-    addTemplate("dsu", "a.cpp");
+    addTemp("dsu", "a.cpp");
 }
 
 int main() {
-    code();
-    int t = 1;
-    // cin >> t;
-
-    while (t--) {
-        solve();
-    }
-
+    solve();
     return 0;
 }
+
---
+++ addTemplate.h
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+string removespace(string s){
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ' ') {
+            s.erase(s.begin() + i);
+            i--;
+        }
+    }
+
+    return s;
+}
+
+void addTemplate(string from, string to){
+    string txt, dir = "Templates/";
+    ifstream Template(dir + from),  curr(to); 
+    ofstream tmp("tmp.txt"); 
+
+    while(getline(curr, txt)){
+        tmp << txt << "\n";
+        if (removespace(txt) == "usingnamespacestd;"){
+            while(getline(Template, txt)){
+                tmp << txt << "\n";
+            }
+        }
+    }
+
+    Template.close();
+    curr.close();
+    tmp.close();
+
+    ifstream newFile("tmp.txt");
+    ofstream afterInsertion(to); 
+
+    while(getline(newFile, txt)){
+        afterInsertion << txt << "\n";
+    }
+
+    afterInsertion.close();
+    newFile.close();
+    remove("tmp.txt");
+}
